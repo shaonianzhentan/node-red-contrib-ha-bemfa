edb09c8fb96ec3d876d08276e66af76b3d4c1eca
shaonianzhentan <ilovevar@gmail.com>
2021-07-16 18:14:38 +0800

    测试完成

--- ha-bemfa.js
+++ ha-bemfa.js
@@ -5,13 +5,38 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         this.server = RED.nodes.getNode(config.server);
         const hass = RED.nodes.getNode(config.hass);
-
+        const node = this
         if (this.server && hass) {
             this.server.register(this)
             const ha = new HomeAssistant(this.server.clientid, hass.hass)
 
-            ha.getEntity().then(() => {
-                this.status({ fill: "blue", shape: "ring", text: "配置成功" });
+            node.on('input', function (msg) {
+                const { payload } = msg
+                this.getBemfaDevice(true)
+            })
+
+            ha.getBemfaDevice().then((entitys) => {
+                entitys.forEach(({ topic_id, domain, entity_id }) => {
+                    this.server.subscribe(topic_id, { qos: 0 }, (mtopic, mpayload, mpacket) => {
+                        const payload = mpayload.toString()
+                        const arr = payload.split('#')
+                        const data = { entity_id }
+                        if (arr.length === 2) {
+                            data['brightness_pct'] = arr[1]
+                        }
+                        // 调用服务
+                        const service = `${domain}.turn_${arr[0]}`
+                        node.send({
+                            service,
+                            data,
+                            payload
+                        })
+                        hass.callService(service, data).then(() => {
+                            node.status({ fill: "green", shape: "ring", text: `调用服务：${service}` });
+                        })
+                    })
+                })
+                this.status({ fill: "green", shape: "ring", text: "配置成功" });
             })
 
         } else {
--- ha.js
+++ ha.js
@@ -5,6 +5,10 @@ function md5(content) {
     return crypto.createHash('md5').update(content).digest("hex")
 }
 
+function log() {
+    console.log(`【${new Date().toLocaleString()}】`, ...arguments)
+}
+
 module.exports = class {
 
     constructor(clientId, hass) {
@@ -13,56 +17,60 @@ module.exports = class {
         this.log = log
     }
 
-    // 获取所有实体
-    async getEntity() {
+    // 获取可用实体列表
+    async getEntityList() {
+        const arr = await this.hass.states.list()
+        return arr.filter((state) => {
+            const { entity_id, attributes } = state
+            const name = attributes['friendly_name']
+            const domain = entity_id.split('.')[0]
+            // 名称是全中文
+            if (!/^[\u4E00-\u9FA5]+$/.test(name)
+                || ['light', 'input_boolean', 'switch'].includes(domain) === false
+            ) return false
+            return true
+        })
+    }
 
+    // 更新巴法云设备
+    async getBemfaDevice(isAll = false) {
+        // 获取所有设备
         const res = await axios.get(`https://go.bemfa.com/v1/getmqttdata?&uid=${this.clientId}`)
         const topic_list = res.data.data.map(ele => ele.topic_id)
-
-        const entity = {}
-        const arr = await this.hass.states.list()
-        arr.forEach((state) => {
+        // 格式化信息
+        const entityList = await this.getEntityList()
+        const list = entityList.map(state => {
             const { entity_id, attributes } = state
             const name = attributes['friendly_name']
             const domain = entity_id.split('.')[0]
             let uuid = md5(entity_id).substring(10, 23)
-            // 只有中文名称才行
-            if (/^[\u4E00-\u9FA5]+$/.test(name) == false) {
-                return
-            }
+            let topic_id = ''
             // 灯
             if (domain === 'light' || (['input_boolean', 'switch'].includes(domain) && name.includes('灯'))) {
-                const topic = uuid + '002'
-                console.log(topic, domain, name)
-                entity[topic] = {
-                    name,
-                    domain,
-                    entity_id
-                }
-                return
+                topic_id = uuid + '002'
             }
-            // 插座
-            if (['input_boolean', 'switch'].includes(domain)) {
-                const topic = uuid + '001'
-                console.log(topic, domain, name)
-                entity[topic] = {
-                    name,
-                    domain,
-                    entity_id
-                }
-                return
+            else if (['input_boolean', 'switch'].includes(domain)) {
+                topic_id = uuid + '001'
+            }
+            return {
+                topic_id,
+                name,
+                domain,
+                entity_id
             }
         })
-        // 添加设备
-        await Promise.all(Object.keys(entity).map(key => {
-            return this.addTopic(key, entity[key].name, topic_list)
-        }))
-        this.entity = entity
-        return entity
+        // 获取可用设备
+        if (isAll) {
+            await Promise.all(list.map(({ topic_id, name }) => {
+                return this.addTopic(topic_id, name, topic_list)
+            }))
+            return list
+        }
+        return list.filter(ele => topic_list.includes(ele.topic_id))
     }
 
     // 添加订阅
-    addTopic(topic, name, topic_list) {
+    async addTopic(topic, name, topic_list) {
         let res = null
         // 删除主题
         // res = await axios.get(`https://go.bemfa.com/v1/deltopic?umail=${this.clientId}&vtype=1&topic=${topic}`)
@@ -82,5 +90,4 @@ module.exports = class {
         res = await axios.get(`https://go.bemfa.com/v1/setname?umail=${this.clientId}&vtype=1&topic=${topic}&name=${encodeURIComponent(name)}`)
         this.log('设置名称', res.data)
     }
-
 }
