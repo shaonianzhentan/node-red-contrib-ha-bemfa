a6cdb866dcfd6b571179b2d4deb0566b4de4ef4b
zhulu <shaonianzhentan@sina.cn>
2021-07-25 22:11:42 +0800

    解决订阅问题

--- ha-bemfa.js
+++ ha-bemfa.js
@@ -16,26 +16,31 @@ module.exports = function (RED) {
             // 需要连接成功了，才执行订阅
             setTimeout(() => {
                 ha.getBemfaDevice().then((entitys) => {
-                    entitys.forEach(({ topic_id, domain, entity_id }) => {
-                        console.log('訂閲', topic_id)
-                        node.server.subscribe(topic_id, { qos: 0 }, (mtopic, mpayload, mpacket) => {
-                            const payload = mpayload.toString()
-                            const arr = payload.split('#')
-                            const data = { entity_id }
-                            if (arr.length === 2) {
-                                data['brightness_pct'] = arr[1]
-                            }
-                            // 调用服务
-                            const service = `${domain}.turn_${arr[0]}`
-                            node.send({
-                                service,
-                                data,
-                                payload
-                            })
-                            hass.callService(service, data).then(() => {
-                                node.status({ fill: "green", shape: "ring", text: `调用服务：${service}` });
-                            })
+                    // 监听服务
+                    node.server.client.on('message', (mtopic, mpayload, mpacket) => {
+                        console.log(mtopic, mpayload)
+                        const { domain, entity_id } = entitys[mtopic]
+                        const payload = mpayload.toString()
+                        const arr = payload.split('#')
+                        const data = { entity_id }
+                        if (arr.length === 2) {
+                            data['brightness_pct'] = arr[1]
+                        }
+                        // 调用服务
+                        const service = `${domain}.turn_${arr[0]}`
+                        node.send({
+                            service,
+                            data,
+                            payload
                         })
+                        hass.callService(service, data).then(() => {
+                            node.status({ fill: "green", shape: "ring", text: `调用服务：${service}` });
+                        })
+                    });
+                    // 订阅
+                    entitys.forEach(({ topic_id }) => {
+                        console.log('訂閲', topic_id)
+                        node.server.client.subscribe(topic_id, { qos: 0 });
                     })
                     this.status({ fill: "green", shape: "ring", text: "配置成功" });
                 })
